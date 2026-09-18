86eea043e79f78fe027e00fb4232ab4fa88c6596
actforfoto-rgb <actforfoto@gmail.com>
2026-09-18 22:58:45 +0300

    Make EPGU response tests Android org.json compatible

--- app/src/test/java/ru/komus/etrnprobe/EpguV114ResponseContractTest.java
+++ app/src/test/java/ru/komus/etrnprobe/EpguV114ResponseContractTest.java
@@ -5,16 +5,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EpguV114ResponseContractTest {
-    @Test public void pushOrderIdAcceptsIntegerAndDigitString(){
+    @Test public void pushOrderIdAcceptsIntegerAndDigitString()throws Exception{
         assertEquals(77,EpguV114ResponseContract.parsePushOrderId(new JSONObject().put("orderId",77)));
         assertEquals(88,EpguV114ResponseContract.parsePushOrderId(new JSONObject().put("orderId","88")));
     }
-    @Test public void pushOrderIdRejectsMissingFractionalAndZero(){
+    @Test public void pushOrderIdRejectsMissingFractionalAndZero()throws Exception{
         assertThrows(IllegalArgumentException.class,()->EpguV114ResponseContract.parsePushOrderId(new JSONObject()));
         assertThrows(IllegalArgumentException.class,()->EpguV114ResponseContract.parsePushOrderId(new JSONObject().put("orderId",1.5)));
         assertThrows(IllegalArgumentException.class,()->EpguV114ResponseContract.parsePushOrderId(new JSONObject().put("orderId",0)));
     }
-    @Test public void parsesStringifiedOrderAndResponseFiles(){
+    @Test public void parsesStringifiedOrderAndResponseFiles()throws Exception{
         JSONObject nested=new JSONObject()
             .put("id",123).put("eserviceId","60025907")
             .put("currentStatusHistoryId",9001).put("orderStatusId",42).put("orderStatusName","Подписано")
@@ -32,7 +32,7 @@ public class EpguV114ResponseContractTest {
         assertTrue(d.responseFiles.get(0).downloadPath(9001).contains("mnemonic=piev_epgu.zip"));
         assertTrue(d.responseFiles.get(0).downloadPath(9001).contains("eserviceCode=60025907"));
     }
-    @Test public void parsesObjectAndLegacyDirectShapes(){
+    @Test public void parsesObjectAndLegacyDirectShapes()throws Exception{
         JSONObject current=new JSONObject().put("id",91).put("statusId",7).put("title","Готово").put("finalStatus",true);
         JSONObject o=new JSONObject().put("orderId","5").put("serviceCode","60025907").put("currentStatusHistory",current);
         assertEquals(5,EpguV114ResponseContract.parseOrder(new JSONObject().put("order",o)).orderId);
@@ -40,7 +40,7 @@ public class EpguV114ResponseContractTest {
         assertEquals(Long.valueOf(91),d.currentStatusHistoryId);assertEquals(Integer.valueOf(7),d.statusId);
         assertEquals("Готово",d.statusName);
     }
-    @Test public void rejectsWrongServiceAndDuplicateResultNames(){
+    @Test public void rejectsWrongServiceAndDuplicateResultNames()throws Exception{
         JSONObject wrong=new JSONObject().put("id",1).put("eserviceId","OTHER");
         assertThrows(IllegalArgumentException.class,()->EpguV114ResponseContract.parseOrder(wrong));
         JSONObject dup=new JSONObject().put("id",1).put("eserviceId","60025907")
@@ -49,7 +49,7 @@ public class EpguV114ResponseContractTest {
                 .put(new JSONObject().put("fileName","x.zip").put("link","https://x/type2")));
         assertThrows(IllegalArgumentException.class,()->EpguV114ResponseContract.parseOrder(dup));
     }
-    @Test public void objectTypeSupportsTerrabyteAndOrdinaryLinks(){
+    @Test public void objectTypeSupportsTerrabyteAndOrdinaryLinks()throws Exception{
         assertEquals("type 3",EpguV114ResponseContract.objectType("terrabyte://host/path/file.zip/type%203"));
         assertEquals("type",EpguV114ResponseContract.objectType("https://host/path/type?x=1"));
     }
